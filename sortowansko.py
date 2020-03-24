c770de57d51030ef70ec5a021683ca893fabddc0
Oskar Bartosz <oskar000001@interia.pl>
2020-03-24 12:57:38 +0100

    Quicksort

--- sortowansko.py
+++ sortowansko.py
@@ -11,7 +11,35 @@ def bubblesort():
 def selectionsort():
     pass
 
+def show_me(table, a, b):
+    print(table)
+    print(a)
+    print(b)
 
-def quicksort():
-    pass
+
+    
+  
+
+def quicksort(table,low,high): 
+    if low < high:
+        i = ( low-1 )
+        pivot = table[high]    
+        for j in range(low , high): 
+            if table[j] <= pivot: 
+                i = i+1 
+                table[i],table[j] = table[j],table[i] 
+        table[i+1],table[high] = table[high],table[i+1] 
+        pi = i+1 
+        quicksort(table, low, pi-1) 
+        quicksort(table, pi+1, high) 
+
+  
+table = [5,2,7,1,9,2]
+print(quicksort(table, 0, len(table)-1))   
+print(table)
+
+
+    
+
+    
 
