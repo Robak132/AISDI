be01209d9eb3149f267ba43b734796e3a2d9010a
Jakub Robaczewski <j.robaczewski@stud.elka.pw.edu.pl>
2020-03-24 11:06:40 +0100

    load

--- sortowansko.py
+++ sortowansko.py
@@ -15,3 +15,15 @@ def selectionsort():
 def quicksort():
     pass
 
+
+def load(file_name):
+    with open(file_name, encoding="utf-8") as file:
+        lines = [line.strip() for line in file.readlines() if line.strip() != ""]
+        words = []
+        for line in lines:
+            words += line.split(" ")
+        return words
+
+
+if __name__ == "__main__":
+    words = load("pan-tadeusz.txt")
