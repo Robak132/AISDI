fe94857db3e18a789cd13fb4dfca416d79a86485
Oskar Bartosz <oskar000001@interia.pl>
2020-03-24 11:07:48 +0100

    Babelki dzialajo

--- sortowansko.py
+++ sortowansko.py
@@ -4,10 +4,17 @@ Jakub Robaczewski, Oskar Bartosz
 """
 
 
-def bubblesort():
-    pass
-
-
+def bubblesort(table):
+    copy_table = table
+    for i in range(len(copy_table)):
+        for j in range(1, len(copy_table)-i):
+             if copy_table[j] < copy_table[j-1]:
+                 boot = copy_table[j]
+                 copy_table[j] = copy_table[j-1]
+                 copy_table[j-1] = boot
+    return copy_table
+    
+        
 def selectionsort():
     pass
 
