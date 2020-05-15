e31da6ce9c040b13d8f44329338e54252fd2434f
Jakub Robaczewski <j.robaczewski@stud.elka.pw.edu.pl>
2020-05-15 19:28:27 +0200

    merge

--- Wzorcowansko/Wzorce.py
+++ Wzorcowansko/Wzorce.py
@@ -6,7 +6,43 @@ def dictify(string):
     for value, letter in enumerate(string[::-1]):
         if letter not in temp_dict.keys():
             temp_dict.update({letter: value})
-    return dict
+    return temp_dict
+
+
+def boilerMur(find, text):
+    dicty = dictify(find)
+    print(dicty)
+    F = len(text)
+    T = len(find)
+    ans_tab = []
+    if F==0 or T==0:
+        return ans_tab
+    index = 0
+    while index <= F-T:
+        j = T-1
+        while j>=0 and find[j] == text[index+j]:
+            j -= 1
+        if (j == -1): 
+            ans_tab.append(index)
+            index += 1
+        else: 
+            if text[index+j] in dicty.keys():
+                index += max(dicty[text[index+j]], 1)
+            else: index += j+1   
+    return ans_tab
+
+
+def testBoilerMur():
+    pure_thruth = []
+    pure_thruth.append(boilerMur("cdeca", "cdeca") == [0])
+    pure_thruth.append(boilerMur("cdeca", "cdec") == [])
+    pure_thruth.append(boilerMur("cdeca", "bcdecab") == [1])
+    pure_thruth.append(boilerMur("", "") == [])
+    pure_thruth.append(boilerMur("hmm", "") == [])
+    pure_thruth.append(boilerMur("", "hmm") == [])
+    pure_thruth.append(boilerMur( "decde","abcdecdecde" ) == [3,6])
+    pure_thruth.append(boilerMur( "aaa","aaaaaaaaaaa" ) == [0,1,2,3,4,5,6,7,8])
+    print(pure_thruth)
 
 
 def special_algorithm(string, text):
