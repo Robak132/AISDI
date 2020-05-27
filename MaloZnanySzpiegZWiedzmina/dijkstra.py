0e08621c5f9b4221c0e1d6204a7c5ea01ef0f3c3
Jakub Robaczewski <j.robaczewski@stud.elka.pw.edu.pl>
2020-05-27 17:04:49 +0200

    Finish

--- MaloZnanySzpiegZWiedzmina/dijkstra.py
+++ MaloZnanySzpiegZWiedzmina/dijkstra.py
@@ -7,61 +7,52 @@ class Graph:
         self.start, self.end = self.searchForZeros(self.map)
 
         self.basic_list = []
-        self.distances = [
-            [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0]
-        ]
+        self.distances = [[0, 0, 0, 0, 0] for i in range(len(self.map))] 
         for y in range(6):
             for x in range(6):
-                self.distances[x][y] = Node(math.inf, y, x)
+                self.distances[y][x] = Node(math.inf, y, x)
                 if (x, y) == self.start:
-                    self.distances[x][y].value = 0
-                self.basic_list.append(self.distances[x][y])
+                    self.distances[y][x].value = 0
+                self.basic_list.append(self.distances[y][x])
+        self.basic_list = sorted(self.basic_list)
 
-        while len(sorted(self.basic_list) != 0):
+        while len(self.basic_list) != 0:
             active_node = self.basic_list[0]
             for neighbour in self.findNeighbour(active_node):
-                if self.distances[neighbour.x][neighbour.y].value > self.distances[active_node.x][active_node.y].value + map[neighbour.x][neighbour.y]:
-                    self.distances[neighbour.x][neighbour.y].value = self.distances[active_node.x][active_node.y].value + map[neighbour.x][neighbour.y]
-                    for elem in self.basic_list:
-                        if elem.x == neighbour.x and elem.y == neighbour.y:
-                            elem.value = self.distances[active_node.x][active_node.y].value + map[neighbour.x][neighbour.y]
+                if (neighbour.value > active_node.value + int(self.map[neighbour.y][neighbour.x])) or math.isinf(neighbour.value):
+                    neighbour.value = active_node.value + int(self.map[neighbour.y][neighbour.x])
                     neighbour.precursor = active_node
 
-            if active_node.x == self.end[0] and active_node.y == self.end[1]:
-                coordinate_list = []
-                coordinate_list.append(active_node.y, active_node.x)
-                next_one = active_node.precursor
-                while next_one != 0:
-                    coordinate_list.append(next_one.y, next_one.x)
-                    next_one = next_one.preprecursor
-                break
-
             self.basic_list.pop(0)
+            self.basic_list = sorted(self.basic_list)
 
+        x, y = self.end
+        next_one = self.distances[y][x]
+        coordinate_list = [(self.start[1], self.start[0])]
+        # coordinate_list.append((active_node.y, active_node.x))
+        # next_one = active_node.precursor
+        while next_one.value != 0:
+            coordinate_list.append((next_one.y, next_one.x))
+            next_one = next_one.precursor
         for y in range(6):
             for x in range(6):
-                #if (y,x) in 
-                pass
-
-        
-
-
+                if (y, x) in coordinate_list:
+                    print(f"{self.map[y][x]}", end="")
+                else:
+                    print("•", end="")
+            print("")
+        print(coordinate_list)
 
     def findNeighbour(self, node):
         connections = []
         if node.y > 0:
-            connections.append(self.map[node.y-1][node.x])
+            connections.append(self.distances[node.y - 1][node.x])
         if node.y < len(self.map[0]) - 1:
-            connections.append(self.map[node.y+1][node.x])
+            connections.append(self.distances[node.y + 1][node.x])
         if node.x > 0:
-            connections.append(self.map[node.y][node.x-1])
+            connections.append(self.distances[node.y][node.x - 1])
         if node.x < len(self.map) - 1:
-            connections.append(self.map[node.y][node.x+1])
+            connections.append(self.distances[node.y][node.x + 1])
         return connections
 
     def load(self, name):
@@ -76,7 +67,7 @@ class Graph:
         for y, row in enumerate(twoDtab):
             for x, elem in enumerate(row):
                 if elem == "0":
-                    answers_tab.append((x,y))
+                    answers_tab.append((x, y))
         return answers_tab
 
 
@@ -86,33 +77,14 @@ class Node:
         self.precursor = None
         self.y = y
         self.x = x
-        self.connections = []
-        #self.visiblity = false
 
-    def __str__(self):
-        if self.visibility:
-            return str(self.value)
-        else:
-            return ""
-    
     def __repr__(self):
-        return self.value
+        return str(self.value)
 
-
-
-#def find_way(start, end):
-#    pass
-#    #nodes 
-
-#def create_list(twoDtab, start):
-#    queue = []
-
+    def __lt__(self, sth):
+        return (self.value < sth.value)
 
 
 if __name__ == "__main__":
     Graph("MaloZnanySzpiegZWiedzmina//mapa.txt")
     pass
-
-
-
-                
